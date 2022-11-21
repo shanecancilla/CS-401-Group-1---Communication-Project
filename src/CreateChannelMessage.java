1f7c2c2da04a070d77330450bdadf4e4e1da931b
Aftersol <42478428+Aftersol@users.noreply.github.com>
2022-11-21 02:27:50 -0800

    create channel message implementation

--- src/CreateChannelMessage.java
+++ src/CreateChannelMessage.java
@@ -1,7 +1,31 @@
 
 public class CreateChannelMessage implements NetworkMessage {
+    NetworkMessageType type;
+    String message;
+
+    public void setNetworkMessageType()
+    {
+        this.type = NetworkMessageType.CreateChannel;
+    }
+
+    public NetworkMessageType getNetworkMessageType()
+    {
+        return this.type;
+    }
+
+    public void setMessage(String newString)
+    {
+        this.message = newString;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
     CreateChannelMessage(String msg)
     {
         this.message = msg;
+        setNetworkMessageType();
     }
 }
