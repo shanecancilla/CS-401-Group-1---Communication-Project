bbf44d22d3c0f5627989ee78345e89261e6017ea
Aftersol <42478428+Aftersol@users.noreply.github.com>
2022-11-23 02:07:48 -0800

    dummy code for logging

---
+++ src/Logging.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logging {
+    List<String> logs;
+
+    Logging()
+    {
+        this.logs = new ArrayList<String>();
+    }
+}
