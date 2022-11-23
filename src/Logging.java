b560d286cb1aea76269054250a2cb90f794acfc7
Aftersol <42478428+Aftersol@users.noreply.github.com>
2022-11-23 02:33:36 -0800

    some logging implementation

--- src/Logging.java
+++ src/Logging.java
@@ -2,7 +2,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Logging {
-    List<String> logs;
+    private List<String> logs;
+
+    public List<String> getLogs()
+    {
+        return logs;
+    }
+
+    public List<String> getFirstLogs(int n)
+    {
+        return logs.subList(logs.size(), logs.size() + n);
+    }
+
+    public List<String> getLogs(int x, int y)
+    {
+        return logs.subList(x, y);
+    }
+
+    public List<String> getLastLogs(int n)
+    {
+        return logs.subList(logs.size() - n, logs.size());
+    }
+
+    public void addLog(String log)
+    {
+        logs.add(log);
+    }
 
     Logging()
     {
